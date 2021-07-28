6b4100a318e32d12aa76d631370a1282fdce00f6
pasca99 <antony.pascalino@mail.polimi.it>
2021-07-28 10:30:53 +0200

    Tried the printf

--- main.c
+++ main.c
@@ -4,5 +4,8 @@ int main()
 {
     printf("Hello, Sicily!\n");
     printf("I'm in a new branch!\n");
+    for (int i = 0; i < 3; i++) {
+        printf("Hers's my number: %d\n", i);
+    }
     return 0;
 }
