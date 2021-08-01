1b2993cbaa0df3001d17c2e2bc7454aef64d4cf8
pasca99 <antony.pascalino@mail.polimi.it>
2021-08-01 12:09:53 +0200

    Ho iniziato a provare a scrivere una lista concatenata

--- main.c
+++ main.c
@@ -2,21 +2,35 @@
 #include <stdlib.h>
 #include <strings.h>
 
-//metodi
+//tipi definiti
+struct Grafo {
+    int indice;
+    int peso;
+    struct Grafo* next;
+    struct Grafo* prev;
+};
+
+//funzioni
 void stampaGrafo();
 void allocaGrafo();
 void leggiGrafo();
 void leggiComandi();
 void leggiPrimoComando();
-//void calcolaPeso();
-//void aggiornaTop();
-//void stampaTopK();
+int calcolaPeso();
+struct Grafo* creaNodo(int peso);
+void aggiornaClassifica(struct Grafo* grafo);
+//void stampaClassifica();
 int fastAtoi(char numero[11]);
 
 //variabili globali
-int **graph;
+int **matrice;
+int *classifica;
 int d;  //graph dimension
 int k;  //number of graph to show
+int grafiLetti = 0;
+int topGrafo = -1; //Il grafo che attualmente ha il peso minore (variabile usata per capire se aggiungere in testa o meno)
+struct Grafo* testa = NULL;
+struct Grafo* curr;
 
 int main() {
     leggiPrimoComando();
@@ -39,8 +53,8 @@ void leggiComandi() {
         if(strcmp(comando, "AggiungiGrafo\n") == 0) {
             printf("Ho letto AggiungiGrafo\n\n");
             leggiGrafo();
-//            calcolaPeso();
-//            aggiornaTop();
+            aggiornaClassifica(creaNodo(calcolaPeso()));
+            grafiLetti++;
         }
         else if(strcmp(comando, "TopK\n") == 0) {
             printf("Ho letto TopK\n\n");
@@ -53,9 +67,9 @@ void leggiComandi() {
 }
 
 void allocaGrafo() {
-    graph = (int **) malloc(d * sizeof(int *));
+    matrice = (int **) malloc(d * sizeof(int *));
     for(int i = 0; i < d; ++i) {
-        graph[i] = (int *) malloc(d * sizeof(int));
+        matrice[i] = (int *) malloc(d * sizeof(int));
     }
     printf("Ho allocato un grafo %dx%d\n\n", d,d);
 }
@@ -64,7 +78,7 @@ void stampaGrafo() {
 
     for(int row = 0; row < d; row++) {
         for(int col = 0; col < d; col++) {
-            printf("%d ", graph[row][col]);
+            printf("%d ", matrice[row][col]);
         }
         printf("\n");
     }
@@ -77,11 +91,11 @@ void leggiGrafo() {
     int currRowChar = 0, currNumberDigit = 0, currReadNumber = 0;
 
     for(int rows = 0; rows < d; rows++) {
-        fgets(riga, 11 * d, stdin); //10 è il numero massimo di cifre che può avere un numero a 32 bit; 10+1=11 per la virgola (o \n) che ogni numero ha
+        fgets(riga, 11 * d + 1, stdin); //10 è il numero massimo di cifre che può avere un numero a 32 bit; 10+1=11 per la virgola (o \n) che ogni numero ha, il +1 final eè richiesto  da fgets
         for(currRowChar = 0, currNumberDigit = 0, currReadNumber = 0;; currRowChar++) {
             if(riga[currRowChar] == ',' || riga[currRowChar] == '\n') {
                 //Converti e salva il numero con atoi
-                graph[rows][currReadNumber] = fastAtoi(numero);
+                matrice[rows][currReadNumber] = fastAtoi(numero);
                 memset(numero, '\0', 11);
                 currNumberDigit = 0;
                 //Se è stata raggiunta la fine della riga
@@ -107,3 +121,42 @@ int fastAtoi(char numero[11]) {
     }
     return convertedNumber;
 }
+
+int calcolaPeso() {
+    return grafiLetti;
+}
+
+struct Grafo* creaNodo(int peso) {
+    struct Grafo* nuovoGrafo;
+
+    nuovoGrafo = (struct Grafo*)malloc(sizeof(struct Grafo));
+    nuovoGrafo->indice = grafiLetti;
+    nuovoGrafo->peso = peso;
+    return nuovoGrafo;
+}
+
+void aggiornaClassifica(struct Grafo* nuovoGrafo) {
+    if(testa == NULL) {
+        testa = nuovoGrafo;
+    }
+    else {
+        curr = testa;
+        while(curr->next->peso <= nuovoGrafo->peso && curr->next != NULL) {
+            curr = curr->next;
+        }
+        if(curr->next == NULL && grafiLetti < k) {
+            curr->next = nuovoGrafo;
+            nuovoGrafo->next = NULL;
+        }
+        else if(curr->next == NULL && grafiLetti >= k) {
+            return;
+        }
+        else if(curr->next != NULL) {
+            nuovoGrafo->next = curr->next;
+
+        }
+    }
+}
+
+
+
